a666d82e9bd7c9347495da4a2873e0bf5b92ab3c
Bin Xu <binxu.nyc@gmail.com>
2018-12-28 22:26:18 -0500

    first commit

---
+++ Assets/MyPlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerController : MonoBehaviour
+{
+    public int speed = 0;
+    private float moveHorizontal, moveVertical;
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        position = transform.position;
+        position.x += moveHorizontal * speed * Time.deltaTime;
+        position.z += moveVertical * speed * Time.deltaTime;
+        transform.position = position;
+    }
+}
